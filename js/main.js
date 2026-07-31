e1210c485e2e865f48b622f21b5bd6bd12d6e2a8
Mohamed Ahmed <mohamed.ahmed.programer@gmail.com>
2026-07-31 23:16:15 +0300

    Display products from API

--- js/main.js
+++ js/main.js
@@ -1,9 +1,26 @@
-async function getProducts()
-{
-  const respons = await fetch("https://dummyjson.com/products")
+async function getProducts() {
 
-  const products = await respons.json();
+    const respons = await fetch("https://dummyjson.com/products");
 
-  console.log(products);
+    const products = await respons.json();
+
+    console.log(products.products[0]);
+    console.log(products.products[0].title);
+    console.log(products.products[0].price);
+    console.log(products.products[0].thumbnail);
+    console.log(products);
+
+  
+  
+    const productsContainer = document.querySelector("#products");
+
+    products.products.forEach(function(product) {
+
+        productsContainer.innerHTML += `
+            <h2>${product.title}</h2>
+        `;
+
+    });
 }
+
 getProducts();
