e52a64799cffe227196bbe01df6c4ef8a6c54fe1
Mohamed Ahmed <mohamed.ahmed.programer@gmail.com>
2026-07-31 23:39:55 +0300

    Fetch and display products from API

--- js/main.js
+++ js/main.js
@@ -1,26 +1,39 @@
 async function getProducts() {
+  const respons = await fetch("https://dummyjson.com/products");
 
-    const respons = await fetch("https://dummyjson.com/products");
+  const products = await respons.json();
 
-    const products = await respons.json();
+  console.log(products.products[0]);
+  console.log(products.products[0].title);
+  console.log(products.products[0].price);
+  console.log(products.products[0].thumbnail);
+  console.log(products);
 
-    console.log(products.products[0]);
-    console.log(products.products[0].title);
-    console.log(products.products[0].price);
-    console.log(products.products[0].thumbnail);
-    console.log(products);
+  const productsContainer = document.querySelector("#products");
 
-  
-  
-    const productsContainer = document.querySelector("#products");
+  products.products.forEach(function (product) {
 
-    products.products.forEach(function(product) {
+    productsContainer.innerHTML += `
+      <div>
+        <img src="${product.thumbnail}" width="200">
 
-        productsContainer.innerHTML += `
-            <h2>${product.title}</h2>
-        `;
+        <h2>${product.title}</h2>
 
-    });
+        <p>Category: ${product.category}</p>
+
+        <p>Brand: ${product.brand}</p>
+
+        <p>⭐ ${product.rating}</p>
+
+        <p>Price: $${product.price}</p>
+
+        <p>Discount: ${product.discountPercentage}% OFF</p>
+
+        <p>Stock: ${product.stock}</p>
+      </div>
+    `;
+
+  });
 }
 
 getProducts();
